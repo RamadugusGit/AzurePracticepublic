99d033546379fb5d8b867f83c9074888d6e9f0f2
RamadugusGit <RamadugusGit>
2023-09-11 14:08:45 +0000

    Push

---
+++ 2023-09-11 - DBFS Example.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC ## Overview
+# MAGIC
+# MAGIC This notebook will show you how to create and query a table or DataFrame that you uploaded to DBFS. [DBFS](https://docs.databricks.com/user-guide/dbfs-databricks-file-system.html) is a Databricks File System that allows you to store data for querying inside of Databricks. This notebook assumes that you have a file already inside of DBFS that you would like to read from.
+# MAGIC
+# MAGIC This notebook is written in **Python** so the default cell type is Python. However, you can use different languages by using the `%LANGUAGE` syntax. Python, Scala, SQL, and R are all supported.
+
+# COMMAND ----------
+
+# File location and type
+file_location = "/FileStore/tables/Uber.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "True"
+first_row_is_header = "True"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+display(df)
